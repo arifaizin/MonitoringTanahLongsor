61a483debe39bc324d843a87c2395ebe708a6f67
Iav <ade.fajr.ariav@gmail.com>
2019-07-18 10:48:00 +0700

    adding xml, push notif #2

--- app/src/main/java/com/iavariav/monitoringiot/MainActivity.java
+++ app/src/main/java/com/iavariav/monitoringiot/MainActivity.java
@@ -1,14 +1,27 @@
 package com.iavariav.monitoringiot;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.text.TextUtils;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
+
+import com.google.firebase.messaging.FirebaseMessaging;
+import com.iavariav.monitoringiot.helper.Config;
 import com.iavariav.monitoringiot.model.ResponseModel;
 import com.iavariav.monitoringiot.rest.ApiConfig;
 import com.iavariav.monitoringiot.rest.ApiService;
+import com.iavariav.monitoringiot.util.NotificationUtils;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -17,14 +30,57 @@ import retrofit2.Response;
 public class MainActivity extends AppCompatActivity {
 
     private ResponseModel responseModel;
+    private String s;
+    private TextView tvDate;
+    private TextView tvClock;
+    private TextView tvCurahHujan;
+    private TextView tvKadarAir;
+    private TextView tvPergeseran;
+    private TextView tvKemiringan;
+    private TextView tvStatus;
+    private EditText edtRegID;
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private BroadcastReceiver mRegistrationBroadcastReceiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+//        getTime();
 
+        mRegistrationBroadcastReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
 
-        getTime();
+                // checking for type intent filter
+                if (intent.getAction().equals(Config.REGISTRATION_COMPLETE)) {
+                    // gcm successfully registered
+                    // now subscribe to `global` topic to receive app wide notifications
+                    FirebaseMessaging.getInstance().subscribeToTopic(Config.TOPIC_GLOBAL);
+
+                    displayFirebaseRegId();
+
+                } else if (intent.getAction().equals(Config.PUSH_NOTIFICATION)) {
+                    // new push notification is received
+
+                    String message = intent.getStringExtra("message");
+
+                    Toast.makeText(getApplicationContext(), "Push notification: " + message, Toast.LENGTH_LONG).show();
+
+                    edtRegID.setText(message);
+                }
+            }
+        };
+
+        displayFirebaseRegId();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        getData();
     }
 
     private void getTime() {
@@ -40,7 +96,7 @@ public class MainActivity extends AppCompatActivity {
             public void onFinish() {
 //                mTextField.setText("done!");
                 getData();
-                getTime();
+
             }
 
         }.start();
@@ -51,9 +107,16 @@ public class MainActivity extends AppCompatActivity {
         apiService.ambilData().enqueue(new Callback<ResponseModel>() {
             @Override
             public void onResponse(Call<ResponseModel> call, Response<ResponseModel> response) {
-                if (response.isSuccessful()){
+                if (response.isSuccessful()) {
                     responseModel = response.body();
-
+                    tvDate.setText(responseModel.getTanggal());
+                    tvClock.setText(responseModel.getWaktu());
+                    tvCurahHujan.setText(responseModel.getCurahHujan() + "°C");
+                    tvKadarAir.setText(responseModel.getKadarAir()  + "°C");
+                    tvPergeseran.setText(responseModel.getPergeseran() + "°");
+                    tvKemiringan.setText(responseModel.getKemiringan() + "°");
+                    tvStatus.setText(responseModel.getStatus());
+                    getTime();
 
                 }
             }
@@ -64,5 +127,54 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+
+    // Fetches reg id from shared preferences
+    // and displays on the screen
+    private void displayFirebaseRegId() {
+        SharedPreferences pref = getApplicationContext().getSharedPreferences(Config.SHARED_PREF, 0);
+        String regId = pref.getString("regId", null);
+
+        Log.e(TAG, "Firebase reg id: " + regId);
+
+        if (!TextUtils.isEmpty(regId))
+            edtRegID.setText("Firebase Reg Id: " + regId);
+        else
+            edtRegID.setText("Firebase Reg Id is not received yet!");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // register GCM registration complete receiver
+        LocalBroadcastManager.getInstance(this).registerReceiver(mRegistrationBroadcastReceiver,
+                new IntentFilter(Config.REGISTRATION_COMPLETE));
+
+        // register new push message receiver
+        // by doing this, the activity will be notified each time a new message arrives
+        LocalBroadcastManager.getInstance(this).registerReceiver(mRegistrationBroadcastReceiver,
+                new IntentFilter(Config.PUSH_NOTIFICATION));
+
+        // clear the notification area when the app is opened
+        NotificationUtils.clearNotifications(getApplicationContext());
+    }
+
+    @Override
+    protected void onPause() {
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(mRegistrationBroadcastReceiver);
+        super.onPause();
+    }
+
+    private void initView() {
+        tvDate = findViewById(R.id.tvDate);
+        tvClock = findViewById(R.id.tvClock);
+        tvCurahHujan = findViewById(R.id.tvCurahHujan);
+        tvKadarAir = findViewById(R.id.tvKadarAir);
+        tvPergeseran = findViewById(R.id.tvPergeseran);
+        tvKemiringan = findViewById(R.id.tvKemiringan);
+        tvStatus = findViewById(R.id.tvStatus);
+        edtRegID = findViewById(R.id.edtRegID);
     }
 }
